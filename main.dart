9eb8ff69cdf327f82b754a074bb0eb6a457dfc0e
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-02-16 10:27:07 +0300

    33

--- main.dart
+++ main.dart
@@ -1,52 +1,20 @@
-Stream<int> getNumbers() async* {
-  for (int i = 0; i < 7; i++) {
-    yield i;
-    yield i;
-    yield i;
-    await Future.delayed(Duration(seconds: 1));
-    // if (i == 2) {
-    //   throw Exception('An error occurred');
-    // }
-  }
-}
+
+
+import 'dart:async';
 
 void main(List<String> args) async {
-  var myStream = getNumbers();
+  var myStreamController = StreamController<int>();
+  Stream<int> myStream = myStreamController.stream;
+  Sink<int> mySink = myStreamController.sink;
 
-  // myStream.expand((element) => [element, element * 2, 99,'ff']).listen((event) {
-  //   print(event);
-  // });
+  mySink.add(5);
+  Future.delayed(Duration(seconds: 1));
+  mySink.add(10);
+  Future.delayed(Duration(seconds: 1));
+  mySink.add(15);
 
-  // myStream.map((event) => event * 5).listen((event) {
-  //   print(event);
-  // });
-
-  // myStream.where((event) => event % 2 == 0).listen((event) {
-  //   print(event);
-  // });
-
-// myStream.where((event) => event % 2 != 0).listen((event) {
-//     print(event);
-//   });
-
-  // myStream.take(2).listen((event) {
-  //   print(event);
-  // });
-
-  // myStream.skip(2).listen((event) {
-  //   print(event);
-  // });
-
-// myStream.distinct().listen((event) {
-//     print(event);
-//   });
-
-  myStream
-      .skip(2)
-      .map((event) => event * 2)
-      .distinct()
-      .where((event) => event > 3)
-      .listen((event) {
+  myStream.listen((event) {
     print(event);
   });
+  
 }
