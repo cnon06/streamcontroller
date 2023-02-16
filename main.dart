2a2263d15cd266f6168854deedffe963f82bd006
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-02-16 09:03:10 +0300

    33

--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
 Stream<int> getNumbers() async* {
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 7; i++) {
+    yield i;
+    yield i;
     yield i;
     await Future.delayed(Duration(seconds: 1));
     // if (i == 2) {
@@ -9,27 +11,42 @@ Stream<int> getNumbers() async* {
 }
 
 void main(List<String> args) async {
-  var myStream = getNumbers().asBroadcastStream();
+  var myStream = getNumbers();
 
-  myStream.listen((event) {
-    print('Stream1: $event');
+  // myStream.expand((element) => [element, element * 2, 99,'ff']).listen((event) {
+  //   print(event);
+  // });
+
+  // myStream.map((event) => event * 5).listen((event) {
+  //   print(event);
+  // });
+
+  // myStream.where((event) => event % 2 == 0).listen((event) {
+  //   print(event);
+  // });
+
+// myStream.where((event) => event % 2 != 0).listen((event) {
+//     print(event);
+//   });
+
+  // myStream.take(2).listen((event) {
+  //   print(event);
+  // });
+
+  // myStream.skip(2).listen((event) {
+  //   print(event);
+  // });
+
+// myStream.distinct().listen((event) {
+//     print(event);
+//   });
+
+  myStream
+      .skip(2)
+      .map((event) => event * 2)
+      .distinct()
+      .where((event) => event > 3)
+      .listen((event) {
+    print(event);
   });
-
-  myStream.listen((event) {
-    print('Stream2: $event');
-  });
-//  print("first: ${await myStream.first}");
-  // print("isBroadcast: ${await myStream.isBroadcast}");
-  
-  // print("last: ${await myStream.last}");
-
-  // print("length: ${await myStream.length}");
-
-// print("isContains 3: ${await myStream.contains(3)}");
-// print("isContains 6: ${await myStream.contains(6)}");
-// print("any: ${await myStream.any((element) => element==2)}");
-// print("any: ${await myStream.any((element) => element==99)}");
-print("join: ${await myStream.join(',')}");
-
- 
 }
